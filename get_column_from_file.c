b9dee4c50b1ff463675f718e760e5380a7508c41
Ale <alexcoppe@gmail.com>
2019-12-08 23:22:01 +0100

    Added get_column_from_line function to get_column_from_file.c

--- get_column_from_file.c
+++ get_column_from_file.c
@@ -4,18 +4,43 @@
 #include<string.h>
 
 
+char *get_column_from_line(char *line, int column_number) {
+    char *copied_line = NULL;
+    int field_number = 0;
+    char *column = NULL;
+    char *subtoken = NULL;
+    int wanted_field = column_number;
+
+    copied_line = (char *) malloc((int) strlen(line));
+    strncpy(copied_line, line, strlen(line) + 1);
+    field_number = 0;
+    column = strtok(copied_line, "\t");
+    subtoken = column;
+    if (subtoken[strlen(subtoken) - 1] == '\n')
+        subtoken[strlen(subtoken) - 1] = '\0';
+
+    while (column != NULL) {
+        if (subtoken == NULL) {
+            break;
+        }
+        else if (field_number == wanted_field) {
+            if (subtoken[strlen(subtoken) - 1] == '\n')
+                subtoken[strlen(subtoken) - 1] = '\0';
+            printf("%s\n", subtoken);
+        }
+
+        subtoken = strtok(NULL, "\t");
+        field_number++;
+
+    }
+    return copied_line;
+}
+
 int main(int argc, char *argv[]){
     FILE *file = NULL;
     char *line = NULL;
     size_t len = 0;    
     size_t read = 0;
-    char *column = NULL;
-    char *copied_line = NULL;
-    char *token;
-    int field_number = 0;
-    int wanted_field = 1;
-    char *contains_new_line = NULL;
-    char *subtoken = NULL;
 
     /*If the ids file is not present */
     if (!(file = fopen(argv[1], "r"))) {
@@ -23,29 +48,10 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    char *wanted_column = NULL;
     while ((read = getline(&line, &len, file) ) != -1) {
-        copied_line = (char *) malloc((int) strlen(line));
-        strncpy(copied_line, line, strlen(line) + 1);
-        field_number = 0;
-        column = strtok(copied_line, "\t");
-        subtoken = column;
-        if (subtoken[strlen(subtoken) - 1] == '\n')
-            subtoken[strlen(subtoken) - 1] = '\0';
-
-        while (column != NULL) {
-            if (subtoken == NULL) {
-                break;
-            }
-            else if (field_number == wanted_field) {
-                if (subtoken[strlen(subtoken) - 1] == '\n')
-                    subtoken[strlen(subtoken) - 1] = '\0';
-                printf("%s\n", subtoken);
-            }
-            subtoken = strtok(NULL, "\t");
-            field_number++;
-        }
-
-        free(copied_line);
+        wanted_column = get_column_from_line(line, 2);
+        free(wanted_column);
     }
     free(line);
 
