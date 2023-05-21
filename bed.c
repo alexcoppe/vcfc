599a76113e34ba6b230b890836cd2585bb9b12df
Ale <alexcoppe@gmail.com>
2023-05-21 17:46:22 +0200

    BedEntry struct and bed_entries fuction developed in bed.c. Still to split header and src

---
+++ bed.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+#define MAX_LINE_LENGTH 10000
+
+typedef struct {
+    char chromosome[20];
+    int start;
+    int end;
+} BedEntry;
+
+void printBedEntry(BedEntry be){
+    printf("%s\t%d\t%d\n", be.chromosome, be.start, be.end);
+}
+
+BedEntry *bed_entries(char *file_path){
+    FILE *file = NULL;
+    ssize_t read;
+    size_t len = 0;
+    int number_of_chars_in_line = 0;
+    int number_of_separators = 0;
+    char separatpr;
+    char line[MAX_LINE_LENGTH];
+    BedEntry *bedEntries = NULL;
+    int numEntries = 0;
+    int i;
+
+    if (!(file = fopen(file_path, "r"))) { 
+        fprintf(stderr, "Coud not find file: %s\n", file_path);
+        exit(EXIT_FAILURE);
+    }
+    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
+        number_of_chars_in_line = strlen(line);
+        for (int i = 0; i < number_of_chars_in_line; i++){
+            if (line[i] == '\t' | line[i] == ' '){
+                number_of_separators++;
+                if (number_of_separators == 2){
+                    separatpr = line[i];
+                    break;
+                }
+            }
+        }
+        /*if (number_of_separators != 2){*/
+        /*fprintf(stderr, "The %s is not a bed file\n", file_path);*/
+        /*exit(EXIT_FAILURE);*/
+        /*}*/
+        /*number_of_separators = 0;*/
+
+        BedEntry entry;
+
+        sscanf(line, "%s %d %d", entry.chromosome, &entry.start, &entry.end);
+
+        numEntries++;
+        bedEntries = (BedEntry *)realloc(bedEntries, numEntries * sizeof(BedEntry));
+        bedEntries[numEntries - 1] = entry;
+    }
+
+    fclose(file);
+
+    return bedEntries;
+}
+
+int main(int argc, char *argv[]){
+    BedEntry *wanted_ranges = {NULL};
+    int i = 0;
+    char c;
+
+    wanted_ranges =  bed_entries(argv[1]);
+
+    while (wanted_ranges[i].chromosome[0]){
+        printBedEntry(wanted_ranges[i]);
+        i++;
+    }
+
+    free(wanted_ranges);
+
+    return 0;
+}
