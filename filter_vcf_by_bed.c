95d45ee7b5d2e4bff4e4ffd4342aad2554e1844a
Alessandro Coppe <alexcoppe@users.noreply.github.com>
2023-05-30 22:46:50 +0200

    filter_vcf_by_bed.c now has parameter -a for showing the VCF header

--- filter_vcf_by_bed.c
+++ filter_vcf_by_bed.c
@@ -3,6 +3,7 @@
 #include<string.h>
 #include<ctype.h>
 #include "bed.h"
+#include<getopt.h>
 
 int main(int argc, char *argv[]){
     BedEntry *wanted_ranges = {NULL};
@@ -14,17 +15,55 @@ int main(int argc, char *argv[]){
     FILE *file = NULL;
     char chromosome[10];
     int position;
+    int opt;
+    int header = 0;
 
-    if (!(file = fopen(argv[2], "r"))) { 
-        fprintf(stderr, "Coud not find file: %s\n", argv[0]);
+    while ((opt = getopt(argc, argv, "a")) != -1){
+        switch (opt){
+            case 'a':
+                header = 1;
+                break;
+            case '?':
+                printf("Unknown option: %c\n", optopt);
+                break;
+            case ':':
+                printf("Missing arg for %c\n", optopt);
+                break;
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
+
+    if (argc < 1) {
+        fprintf(stderr, "BED file not specified\n");
+        return -1;
+    }
+
+    if (argc < 2) {
+        fprintf(stderr, "VCF file not specified\n");
+        return -1;
+    }
+
+    if (!(file = fopen(argv[0], "r"))) { 
+        fprintf(stderr, "Could not find BED file: %s\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    wanted_ranges = bed_entries(argv[1]);
+    if (!(file = fopen(argv[1], "r"))) { 
+        fprintf(stderr, "Could not find VCF file: %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    wanted_ranges = bed_entries(argv[0]);
 
     while ((read = getline(&line, &len, file)) != -1) {
-        if (line[0] == '#')
+        if (line[0] == '#'){
+            if (header != 0){
+                printf("%s", line);
+            }
             continue;
+        }
         sscanf(line, "%s %i", chromosome, &position);
         i = 0;
         while (wanted_ranges[i].chromosome[0]){
